92c271eea9142e63f168e8b5dcf80cae1d93caea
mukul-bhalla <bhallamukul27@gmail.com>
2024-08-22 22:15:01 +0530

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import About from './Pages/About'
+import Article from './Pages/Article'
+import ArticlesList from './Pages/ArticlesList'
+
+import Navbar from './Components/Navbar'
 function App() {
 
   return (
     <Router>
+      <Navbar></Navbar>
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/article-list' element={<ArticlesList />} />
+          <Route path='/article' element={<Article />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Pages/Article.jsx
@@ -0,0 +1,19 @@
+const Article = () => {
+    return <div className="mb-20">
+        <h1 className="sm:text-4xl text-2xl, font-bold my-6 text-gray-900">Article</h1>
+
+        <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione natus est tenetur aliquam corporis quaerat incidunt inventore officia enim aliquid atque, praesentium harum hic facilis? Modi facere officiis facilis voluptate!
+            Natus necessitatibus possimus suscipit magni quaerat sit rem debitis voluptatum autem consectetur repellat corrupti, maxime explicabo cum quos velit ipsam a illum temporibus nesciunt harum! Cumque minus corrupti iure porro!
+            Consectetur placeat quibusdam ducimus ad ut minima iure deserunt repellendus eum et, tempore, facilis dolorum consequuntur magni ullam corrupti atque dolores ipsum voluptate magnam eligendi sint fugit tempora numquam? Sequi.</p>
+
+        <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione natus est tenetur aliquam corporis quaerat incidunt inventore officia enim aliquid atque, praesentium harum hic facilis? Modi facere officiis facilis voluptate!
+            Natus necessitatibus possimus suscipit magni quaerat sit rem debitis voluptatum autem consectetur repellat corrupti, maxime explicabo cum quos velit ipsam a illum temporibus nesciunt harum! Cumque minus corrupti iure porro!
+            Consectetur placeat quibusdam ducimus ad ut minima iure deserunt repellendus eum et, tempore, facilis dolorum consequuntur magni ullam corrupti atque dolores ipsum voluptate magnam eligendi sint fugit tempora numquam? Sequi.</p>
+
+        <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione natus est tenetur aliquam corporis quaerat incidunt inventore officia enim aliquid atque, praesentium harum hic facilis? Modi facere officiis facilis voluptate!
+            Natus necessitatibus possimus suscipit magni quaerat sit rem debitis voluptatum autem consectetur repellat corrupti, maxime explicabo cum quos velit ipsam a illum temporibus nesciunt harum! Cumque minus corrupti iure porro!
+            Consectetur placeat quibusdam ducimus ad ut minima iure deserunt repellendus eum et, tempore, facilis dolorum consequuntur magni ullam corrupti atque dolores ipsum voluptate magnam eligendi sint fugit tempora numquam? Sequi.</p>
+    </div>
+}
+
+export default Article;
